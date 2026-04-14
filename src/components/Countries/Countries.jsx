4848104cf333203e83bc073c64a03219c7ec1d88
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-14 21:10:05 +0600

    grid

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from '../country/Country';
+import './Countries.css';
 
 const Countries = ({countryPromise}) => {
 
@@ -7,11 +8,13 @@ const Countries = ({countryPromise}) => {
     const countries= countriesAll.countries;
     console.log(countriesAll);
     return (
-        <div>
+        <div >
             <h3>There are {countries.length} countries in the world.</h3>
-            {
+            <div id='countries'>
+                {
                 countries.map(country => <Country country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
 
     return (
         
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
             <h4>Name:{country.name.common} </h4>
             <p>Population:{country.population.population}</p>
